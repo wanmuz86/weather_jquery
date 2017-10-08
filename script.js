6f85bd15d92ae9293a3de6a563d14ab76d047853
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2017-10-08 14:33:15 +0800

    complete

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
+var weatherArray = [];
+var savedWeathers = [];
+
 $(function(){
-	console.log("here 1");
+	retrieveWeathers();
 	if (navigator.geolocation){
-	
 		navigator.geolocation.getCurrentPosition(function(position){
 			console.log(position)
 			console.log(position.coords.latitude)
@@ -12,6 +15,7 @@ $(function(){
 			$.get(url, 
 				function(data, status){
 					console.log(data);
+					weatherArray = data.list
 					$.each(data.list, function(index, value){
 						console.log(new Date(value.dt*1000))
 						$("#weathertablebody").append("<tr><td>"+new Date(value.dt*1000)+'</td><td><img src="https://openweathermap.org/img/w/'+value.weather[0].icon+'.png"></td><td>'+(value.temp.day-273).toFixed(2)+" &deg; C</td><td>"+value.pressure+"</td><td>"+value.humidity+'</td><td><button onclick="saveItem('+index+')">Save</button></td></tr>');
@@ -23,8 +27,42 @@ $(function(){
 
 	}
 });
+
+function retrieveWeathers(){
+
+	savedWeathers = JSON.parse(localStorage.getItem("weathers"));
+	if (!savedWeathers){
+		savedWeathers = [];
+	}
+
+	console.log(savedWeathers);
+	$.each(savedWeathers, function(index, value){
+						console.log(new Date(value.dt*1000))
+						$("#savedtablebody").append("<tr><td>"
+							+new Date(value.dt*1000)+'</td><td><img src="https://openweathermap.org/img/w/'+value.weather[0].icon+'.png"></td><td>'+(value.temp.day-273).toFixed(2)+" &deg; C</td><td>"+value.pressure+"</td><td>"+value.humidity+'</td></tr>');
+		})
+}
 function saveItem(item){
-	console.log(item);
+	var savedItem =  weatherArray[item]
+
+	savedWeathers.push(savedItem);
+	if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("weathers", 
+    	JSON.stringify(savedWeathers));
+
+    $("#savedtablebody").
+    append("<tr><td>"+new Date(savedItem.dt*1000)+
+	'</td><td><img src="https://openweathermap.org/img/w/'
+	+savedItem.weather[0].icon+'.png"></td><td>'
+	+(savedItem.temp.day-273).toFixed(2)+" &deg; C</td><td>"
+	+savedItem.pressure+"</td><td>"+savedItem.humidity
+	+'</td></tr>');
+		
+
+} else {
+    alert("Sorry your browser does not support local storage.")
+}
+
 
 }
 
